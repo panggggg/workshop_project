cedffc7f6ff92a9940e834ef7d377434076e9ef1
panggggg <p.picczy@gmail.com>
2021-03-23 00:16:18 +0700

    edit app.js file

--- src/App.js
+++ src/App.js
@@ -10,8 +10,10 @@ const App = (props) => {
       .then((data) => setData(data.data));
   }, []);
   return (
-    <div>
-      <div className="container" style={{ backgroundImage: "https://image.freepik.com/free-vector/bakery-seamless-pattern-with-cartoon-bread_53562-7503.jpg" }}>
+    <div style={{
+      backgroundImage: `url("https://image.freepik.com/free-vector/bakery-seamless-pattern-with-cartoon-bread_53562-7503.jpg")`
+    }}>
+      <div className="container" >
         <nav class="navbar" style={{ background: "#BE987A" }} >
           <div class="container-fluid" style={{ fontSize: 50, color: "#FFFFFF" }}>
             <a class="navbar-brand">
@@ -20,21 +22,35 @@ const App = (props) => {
     </a>
           </div>
         </nav>
-        <h1 align="center" style={{ fontSize: 60 }}>MENU</h1>
+        <div align="center">
+          <h1 align="center" style={{ fontSize: 60, color: "#492D17", borderRadius: "", border: "2px solid #E1A473", padding: "20px", width: "200px", height: "", backgroundColor: "#E1A473" }}>MENU</h1></div>
         <div align="center">
           {data.map((item) => {
-            return <div class="card">
-              <img src={item.picture_url} class="card-img-top" alt="..." style={{ width: 300, height: 300 }} />
-              <div class="card-body">
-                <h5 class="card-title">{item.menu_name}</h5>
-                <p class="card-text">price: {item.price}</p>
-
+            return <div className="card" style={{}}>
+              <div class="card" style={{ width: "18rem;" }}>
+                <img src={item.picture_url} class="card-img-top" alt="..." style={{ width: 200, height: 200 }} />
+                <div class="card-body">
+                  <p class="card-text" style={{ fontSize: 20, borderRadius: "", border: "2px solid #E1A473", padding: "20px", width: "200px", height: "", backgroundColor: "#E1A473" }}>{item.menu_name}</p>
+                  <p class="card-text" style={{ fontSize: 20 }}>price: {item.price} Baht</p>
+                </div>
+                <br></br>
               </div>
+              {/* // <div class="card" style={{ backgroundColor: "#DCB99D", width: 300, paddingTop: "10px" }}>
+            //   <img src={item.picture_url} class="card-img-top" alt="..." style={{ width: "18rem" }} />
+            //   <div class="card-body">
+            //     <h5 class="card-title" style={{ fontFamily: "cursive", color: "#ffffff", fontSize: 20 }}>{item.menu_name}</h5>
+            //     <h5 class="card-title" style={{ fontFamily: "cursive", color: "#ffffff", fontSize: 20 }}>price: {item.price} Baht</h5>
+
+
+            //   </div>
+            //   <br></br> */}
+
             </div>
+
           })}
         </div>
       </div>
     </div>
-  )
+  );
 }
 export default App;
